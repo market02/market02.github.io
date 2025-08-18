cce6256e09c98575b686b85bad9df980aa64d65a
alwaysgot200 <ricky.ruixiwu@gmail.com>
2025-08-18 23:41:51 +0800

    fix: 修复lint-staged配置中

--- .husky/lintstagedrc.js
+++ .husky/lintstagedrc.js
@@ -1,6 +1,6 @@
 export default {
   "*.{js,jsx,ts,tsx}": ["eslint --fix", "prettier --write"],
-  "!(package)*.json": ["prettier --write--parser json"],
+  "!(package)*.json": ["prettier --write --parser json"],
   "package.json": ["prettier --write"],
   "*.vue": ["eslint --fix", "prettier --write"],
   "*.{vue,css,scss,postcss,less}": ["prettier --write"],
